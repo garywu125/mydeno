e763a45a7d290324ebbf743251c48c173be59ecb
garywu125 <garywu125@gmail.com>
2021-05-10 20:00:06 -0700

    ass

--- deno-basic/shell-tool/dx01.ts
+++ deno-basic/shell-tool/dx01.ts
@@ -17,7 +17,7 @@ console.log("Files count: ", parseInt(output));
 let json = await fetch('https://httpbin.org/ip').then(resp => resp.json());
 console.log("Your ip: ", json.origin)
 
-
+// testing
 
 // // your home dir
 // console.log("Your home: ", os.homedir());
