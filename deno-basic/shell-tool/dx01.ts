a9cf09d5c2c13b9e9742d07697edb1319071908f
garywu125 <garywu125@gmail.com>
2021-05-14 09:18:15 -0700

    20210515

--- deno-basic/shell-tool/dx01.ts
+++ deno-basic/shell-tool/dx01.ts
@@ -3,22 +3,23 @@
 import {$, cd, pwd, question, os, fs, env} from "https://denopkg.com/linux-china/dx/mod.ts";
 import {red, yellow, blue, green} from "https://deno.land/std@0.95.0/fmt/colors.ts";
 
+#!/usr/bin/env dx
+
+import {$, cd, pwd, question, os, fs, env} from "https://denopkg.com/linux-china/dx/mod.ts";
+import {red, yellow, blue, green} from "https://deno.land/std@0.95.0/fmt/colors.ts";
+
 // prompt to input your name
 let name = await question(blue("what's your name: "));
 console.log("Hello ", blue(name ?? "guest"));
 
 console.log("Current working directory:", pwd());
+console.log("Your home:", HOME);
+console.log("Your name:", USER);
 
-// current file count
+// language=bash count files
 const output = await $`ls -1 | wc -l`;
 console.log("Files count: ", parseInt(output));
 
-// // print your internet outbound ip
+// print your internet outbound ip
 let json = await fetch('https://httpbin.org/ip').then(resp => resp.json());
 console.log("Your ip: ", json.origin)
-
-// testing
-
-// // your home dir
-// console.log("Your home: ", os.homedir());
-
